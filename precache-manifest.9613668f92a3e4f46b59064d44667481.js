df043c1e290f53930948ef5ce9c80c193ebbf965
Matvey Malkov <work.malkov.matvey@gmail.com>
2019-02-22 16:25:19 +0500

    Updates

---
+++ precache-manifest.9613668f92a3e4f46b59064d44667481.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "5d773ee0f1b455a7b3ea",
+    "url": "/my-app/static/js/main.5d773ee0.chunk.js"
+  },
+  {
+    "revision": "86fd2c6431b26deabcf5",
+    "url": "/my-app/static/js/1.86fd2c64.chunk.js"
+  },
+  {
+    "revision": "5d773ee0f1b455a7b3ea",
+    "url": "/my-app/static/css/main.82c27985.chunk.css"
+  },
+  {
+    "revision": "9871f0178fc4e314beab4df1d83bbbec",
+    "url": "/my-app/index.html"
+  }
+];
